3a980d180015e75afea486db8bb844053a42eba7
Foddie2 <Bensonagala@live.com>
2022-09-19 22:17:13 +0300

    Generate QR Code

--- app.py
+++ app.py
@@ -2,3 +2,14 @@
 #create a function that collects text and convert to a QR code 
 #Save the QR code as an image 
 #Run the function echo "# Python" >> README.md
+
+from asyncio import constants
+import qrcode
+
+def generate_qrcode(text):
+    qr = qrcode.QRCode(
+        version= 1,
+        error_correction= qrcode.constants.ERROR_CORRECT_l,
+        box_size=10,
+        border=5    
+    )
