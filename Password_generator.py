e679c8b1647bd934db15d527b9f694e5f6c57c7b
Foddie2 <Bensonagala@live.com>
2022-09-25 11:17:53 +0300

    Random Password generator

---
+++ Password_generator.py
@@ -0,0 +1,17 @@
+# Ask the user if they want to generate the password or not
+# If yes , ask for the password length
+# Generate the password d
+# Print the password 
+# If initial response is no , exit the program
+
+
+import string
+import random
+
+characters = list(string.ascii_letters + string.digits + '!@#$%^&*()')
+
+#Create a function
+
+def generate_password():
+        password_length = int(input("How long would you like your password to be? "))
+
