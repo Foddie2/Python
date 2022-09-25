b158b1795b8c8499f6c6f5b4adaeb784dad99e6a
Foddie2 <Bensonagala@live.com>
2022-09-25 14:29:00 +0300

    cool shapes

--- coursera.py
+++ coursera.py
@@ -5,7 +5,7 @@
 
 
 
-total = 2048 + 4357 + 97658 + 125 + 8
-files = 5
-average = total / files
-print("The average size is: " + str(average))
+# total = 2048 + 4357 + 97658 + 125 + 8
+# files = 5
+# average = total / files
+# print("The average size is: " + str(average))
