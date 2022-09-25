12159841d9b87481334084a25ba0571ee11acc36
Foddie2 <Bensonagala@live.com>
2022-09-25 09:14:23 +0300

    cursera-practice

--- coursera.py
+++ coursera.py
@@ -1,3 +1,10 @@
+# 
+# Practice writing some expressions and conversions yourself.
+
+# In this scenario, we have a directory with 5 files in it. Each file has a different size: 2048, 4357, 97658, 125, and 8. Fill in the blanks to calculate the average file size by having Python add all the values for you, and then set the files variable to the number of files. Finally, output a message saying "The average size is: " followed by the resulting number. Remember to use the str() function to convert the number into a string. 
+
+
+
 total = 2048 + 4357 + 97658 + 125 + 8
 files = 5
 average = total / files
