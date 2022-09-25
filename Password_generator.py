55de32cec0cef3bfdfcb86a691ee8c424dbf7014
Foddie2 <Bensonagala@live.com>
2022-09-25 11:19:37 +0300

    Random Password generator

--- Password_generator.py
+++ Password_generator.py
@@ -15,3 +15,7 @@ characters = list(string.ascii_letters + string.digits + '!@#$%^&*()')
 def generate_password():
         password_length = int(input("How long would you like your password to be? "))
 
+        random.shuffle(characters)
+
+        password = []
+
