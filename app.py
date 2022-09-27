f067704cfdebabb2c978eedd7c4321f0d8bf4b30
Foddie2 <Bensonagala@live.com>
2022-09-27 19:28:25 +0300

    configs

--- app.py
+++ app.py
@@ -5,25 +5,25 @@
 #Save the QR code as an image 
 #Run the function echo "# Python" >> README.md
 
-# from asyncio import constants
-# from re import T
-# import qrcode
+from asyncio import constants
+from re import T
+import qrcode
 
-# def generate_qrcode(text):
-#     qr = qrcode.QRCode(
-#         version= 1,
-#         error_correction= qrcode.constants.ERROR_CORRECT_L,
-#         box_size=10,
-#         border=5    
-#     )
+def generate_qrcode(text):
+    qr = qrcode.QRCode(
+        version= 1,
+        error_correction= qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=5    
+    )
 
-#     #Add data to the QR code https://www.facebook.com/benson.agala
+    #Add data to the QR code https://www.facebook.com/benson.agala
 
-#     qr.add_data(text)
-#     qr.make(fit=True)
-#     img = qr.make_image(fill_color='black', back_color='white')
-#     img.save('Twitterhttps://twitter.com/FoddieHurleyhttps://twitter.com/FoddieHurley.png')
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color='black', back_color='white')
+    img.save('Twitterhttps://twitter.com/FoddieHurleyhttps://twitter.com/FoddieHurley.png')
 
-# url = input ('Enter your URL: ')
+url = input ('Enter your URL: ')
 
-# generate_qrcode('url')
+generate_qrcode('url')
