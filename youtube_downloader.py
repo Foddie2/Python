b1456aa16c3c0e617a7bb67f5fb5f441f20e1559
unknown <bensonagala@live.com>
2023-07-03 14:26:53 +0300

    Python youtube_downloader

--- youtube_downloader.py
+++
@@ -1,7 +0,0 @@
-import pytube
-from pytube import YouTube
-
-link = input('Youtube Video URL')
-video_download = pytube.Youtube(link)
-video_download.streams.first().download()
-print('Video Downloaded', link)
