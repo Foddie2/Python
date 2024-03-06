3f7609ee916a20a8d26c868f2e82dc4d83c12fae
unknown <bensonagala@live.com>
2024-03-06 14:22:05 +0300

    cleanup

--- crack.py
+++
@@ -1,8 +0,0 @@
-from string import ascii_letters, digits, punctuation
-
-for i in ascii_letters + digits + punctuation:
-    for j in ascii_letters + digits + punctuation:
-        for k in ascii_letters + digits + punctuation:
-            for l in ascii_letters + digits + punctuation:
-                print(i,j,k,l)
-            
